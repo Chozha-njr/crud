2f69a78dc08d92cfdefb78172c575520650c10b4
chozha <chozha@gmail.com>
2023-12-20 01:35:01 +0530

    commited file

---
+++ Phase_3_project/restAssuredDemo/PUTDemo.java
@@ -0,0 +1,23 @@
+package restAssuredDemo;
+
+import java.util.HashMap;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import io.restassured.RestAssured;
+
+public class PUTDemo {
+	HashMap<String, String> map = new HashMap<String, String>();
+
+	@BeforeMethod
+	public void createPayLoad() {
+		map.put("name", "rajinikanth");
+		map.put("Position", "*Superstar*");
+		RestAssured.baseURI = "https://reqres.in/";
+		RestAssured.basePath = "/api/users/694";
+	}
+
+	@Test
+	public void createResource() {
+		RestAssured.given().contentType("application/json").body(map).when().put().then().statusCode(200).log().all();
+	}
+}
